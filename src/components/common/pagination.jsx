c4717cdd5cf34c49ad656a622b00b2ad00501720
Sudip Sikdar <sudip.sikdar@gmail.com>
2020-09-16 00:14:27 +0800

    Update pagination.jsx

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -3,6 +3,7 @@ import _ from "lodash";
 
 const Pagination = (props) => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
+
   const pagesCount = Math.ceil(itemsCount / pageSize);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
@@ -10,8 +11,11 @@ const Pagination = (props) => {
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className={page === cu}>
-            <a className="page-link" onClick={() => props.onPageChange(page)}>
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
               {page}
             </a>
           </li>
